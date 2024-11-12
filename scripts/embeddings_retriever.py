2177d35e2a018ff0fa6705dd93b86958104fdd5a
chesswondo <chesswondo@gmail.com>
2024-11-12 22:43:21 +0200

    Deleted one flag from retriever

--- scripts/embeddings_retriever.py
+++ scripts/embeddings_retriever.py
@@ -6,8 +6,7 @@ from utils.common_utils import load_config, set_device
 def calculate_embeddings(input_data_path: str,
                          output_data_path: str,
                          embedding_model_config: dict,
-                         device: str,
-                         allow_download: bool) -> None:
+                         device: str) -> None:
 
     try:
         movie_df = pd.read_csv(input_data_path)
@@ -19,7 +18,7 @@ def calculate_embeddings(input_data_path: str,
     embedding_model = EmbeddingModel(model_name=embedding_model_config["model_name"],
                                      cache_dir=embedding_model_config["model_path"],
                                      device=device,
-                                     allow_download=allow_download)
+                                     allow_download=True)
 
     # Compute embeddings for all overviews
     try:
@@ -41,7 +40,7 @@ def calculate_embeddings(input_data_path: str,
 def main():
     parser = argparse.ArgumentParser(description="Calculate embeddings for a dataset")
     parser.add_argument(
-        "--input_file", help="Path to the dataset", required=True, dest="input_file"
+        "--input_file", help="Path to the dataset", required=False, default="../data/IMDB_cleaned.csv", dest="input_file"
     )
     parser.add_argument(
         "--output_file", help="Path to the output file", required=False, default="../data/IMDB_embeddings.pkl", dest="output_file"
@@ -61,14 +60,12 @@ def main():
     input_file = args.input_file
     output_file = args.output_file
     device = set_device(args.device) if args.device != -1 else set_device(embedding_model_config["device"])
-    allow_download = args.allow_download
 
     # Calculate the embeddings
     calculate_embeddings(input_data_path=input_file,
                          output_data_path=output_file,
                          embedding_model_config=embedding_model_config,
-                         device=device,
-                         allow_download=allow_download)
+                         device=device)
 
 if __name__ == "__main__":
     main()
